32826f011bd890b7fe282e5f9cbc680242419206
PratikMane09 <Pratik09092001@gmail.com>
2024-04-14 21:10:44 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlCaSrlOMPQAq4ToB6qGqFp7jwOOce0-4",
+  authDomain: "chat-react-4e809.firebaseapp.com",
+  projectId: "chat-react-4e809",
+  storageBucket: "chat-react-4e809.appspot.com",
+  messagingSenderId: "847075924164",
+  appId: "1:847075924164:web:885fe7d969cdc201ca1191",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
